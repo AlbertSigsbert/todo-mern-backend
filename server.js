8e5dc72f904dcca35e8ef17d1770f1de4a5bb33a
Albert <ishengomaalbert@gmail.com>
2023-02-23 13:32:46 +0300

    Set cors origin

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const todoRoutes = require("./routes/todos");
 const userRoutes = require("./routes/user");
-
 
 //express app
 const app = express();
 
 //midlewares
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://todo-mern-frontend.netlify.app"],
+  })
+);
+
 app.use(express.json());
 
 //routes
 app.use("/api/todos", todoRoutes);
-app.use('/api/user', userRoutes);
+app.use("/api/user", userRoutes);
 
 //connect to db
 mongoose
@@ -21,10 +27,11 @@ mongoose
   .then(() => {
     //listen for request
     app.listen(process.env.PORT, () => {
-      console.log(`Connected to db & listening for request on port ${process.env.PORT}`);
+      console.log(
+        `Connected to db & listening for request on port ${process.env.PORT}`
+      );
     });
   })
   .catch((err) => {
     console.log(err);
   });
-
